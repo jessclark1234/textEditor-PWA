bd6d53003aab8ee84cd865353ea8108b8714cfcb
jessclark1234 <jessicaclark7890@gmail.com>
2024-03-22 20:24:32 -0700

    added to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 
+  console.error('putDb not implemented');
+
+  const callDb = await openDB('content', 1);
+
+  const text = callDb.transaction('content', 'readwrite');
+
+  const storeText = text.objectStore('content');
+
+  const requestText = storeText.add({});
+
+  const result = await requestText;
+  console.log('Saved to database', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const callDb = await openDB('content', 1);
+
+  const text = callDb.transaction('content', 'readwrite');
+
+  const storeText = text.objectStore('content');
+
+  const requestText = storeText.getAll();
+
+  const result = await requestText;
+  console.log('result.value', result);
+  return result;
+}
+
 
 initdb();
