8350d71d0c1d5463754ed6f90e8a52c13e8127b5
jessclark1234 <jessicaclark7890@gmail.com>
2024-03-25 19:16:33 -0700

    final changes push

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,14 +4,14 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-
+    console.log(('beforeinstallprompt'), window.deferredPrompt)
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-
+    console.log(promptEvent)
     if (!promptEvent) {
         return;
     }
@@ -26,4 +26,5 @@ butInstall.addEventListener('click', async () => {
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
+    console.log(window.deferredPrompt);
 });
